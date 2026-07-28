b480751088fd16b5e2a553a4e0fa0412df9c2e09
eitanVIP <eitangrimblat@gmail.com>
2026-07-28 18:03:51 +0300

    Fixing expo ui problem. Not fixed yet

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,6 @@ const config = getDefaultConfig(__dirname);
 
 // Enable .cjs extension support for Firebase SDK
 config.resolver.sourceExts.push('cjs');
-config.resolver.unstable_enablePackageExports = false;
+config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
